4f2a33f062d60f5fc7dfe89f975a7186075fcce9
trungld3 <k21tpm4@gmail.com>
2020-05-22 11:25:47 +0700

    pinned location

--- Finding The User Location/ViewController.swift
+++ Finding The User Location/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation : CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longtitude = userLocation.coordinate.longitude
+        let latDelta : CLLocationDegrees = 0.05
+        let longDelta : CLLocationDegrees = 0.05
+         
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+    }
 }
 
